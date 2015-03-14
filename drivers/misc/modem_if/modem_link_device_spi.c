8862f4479b780e06bee3ba968f65292a3591d63e
Tran Hoang Hiep <hiepgia@hiepgia.com>
2015-03-15 00:54:14 +0700

    optimize and fix

--- drivers/misc/modem_if/modem_link_device_spi.c
+++ drivers/misc/modem_if/modem_link_device_spi.c
@@ -748,7 +748,8 @@ static void spi_rx_work(void)
 
 	if (!wake_lock_active(&spild->spi_wake_lock) ||
 		gpio_get_value(spild->gpio_ipc_srdy) == SPI_GPIOLEVEL_LOW ||
-		get_console_suspended() ||
+		//Fix phone delay
+		//get_console_suspended() ||
 		spild->spi_state == SPI_STATE_END)
 		return;
 
